1adbca415944fe87652b5f2229295e4936666f6a
RobertoSosa25sr <ricardo_sr25@outlook.com>
2025-01-19 13:09:16 -0500

    update password

--- app/Actions/Fortify/UpdateUserPassword.php
+++ app/Actions/Fortify/UpdateUserPassword.php
@@ -3,7 +3,7 @@
 namespace App\Actions\Fortify;
 
 use App\Models\Notification;
-use App\Models\User;
+use Illuminate\Foundation\Auth\User as AuthUser;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\UpdatesUserPasswords;
@@ -17,7 +17,7 @@ class UpdateUserPassword implements UpdatesUserPasswords
      *
      * @param  array<string, string>  $input
      */
-    public function update(User $user, array $input): void
+    public function update(AuthUser $user, array $input): void
     {
         Validator::make($input, [
             'current_password' => ['required', 'string', 'current_password:web'],
